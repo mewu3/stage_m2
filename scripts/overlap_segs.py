1b4e14bd6b64e188471de6af5e5a26d5e2740bc2
Meijun Wu <wu.meijun@outlook.com>
2021-02-23 13:58:53 +0100

    continue developement

--- scripts/overlap_segs.py
+++ scripts/overlap_segs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.8
 import sys
 import numpy as np
 import pandas as pd
@@ -14,10 +14,26 @@ overlap = 250
 
 ### global variables
 input = Fasta(input)
-lenRange = range(0,len(input[0]))
+lsRange = list(range(len(input[0])))
+ran = range(len(input[0]))
 
 ### split into overlapping segments
-chunks = [lenRange[i:i+step] for i in range(0, len(input[0]), step-overlap)]
+chunks = [[i,i+step] for i in range(0, len(input[0]), step-overlap)]
 
-print(chunks[0])
-print(input[0][chunks[0]])
+# for id in input.keys(): 
+#     allFoward = []
+#     allReverse = []
+#     for chunk in chunks:
+#         segment = input[id][chunk[0]:chunk[1]]
+#         forward = str(segment[:50])
+#         reverse = str(segment[-50:])
+#         allFoward.append(forward)
+#     print(allFoward)
+
+for chunk in chunks: 
+    for id in input.keys() : 
+        segment = input[id][chunk[0]:chunk[1]]
+        forward = str(segment[:50])
+        reverse = str(segment[-50:])
+        print(forward, end="\n")
+    print()
