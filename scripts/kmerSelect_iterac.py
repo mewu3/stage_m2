02daacb3c7ccc5aa1d458c5dbe01973605aac165
Meijun Wu <wu.meijun@outlook.com>
2021-03-26 16:46:03 +0100

    working on iteraction

--- scripts/kmerSelect_iterac.py
+++ scripts/kmerSelect_iterac.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3.8
 import sys
 import os
+from Bio import SeqIO
+import re
+from collections import Counter
 
 inputfile = sys.argv[1]
-# outputfile = open(sys.argv[2], "w")
-# dataDir = snakemake.params.dataDir
+outputfile = open(sys.argv[2], "w")
 dataDir = "/datater/wu/data"
 sample = "enterovirus"
 seg = "0-504"
+seqNum = int(os.popen(f"grep '^>' -c {inputfile}").read())
+# dataDir = snakemake.params.dataDir
 # sample = snakemake.wildcards.sample
 # seg = snakemake.wildcards.seg
 
-
-
 def calculate_kmer(inputfile):
-    dskH5 = f"{dataDir}/{sample}/dsk/{seg}.h5"
+    dskH5 = f"{dataDir}/{sample}/dsk/{seg}.inter.h5"
     os.system(
         f"lib/dsk/build/bin/dsk \
         -nb-cores 0 \
@@ -30,19 +32,73 @@ def calculate_kmer(inputfile):
         -out {dskH5}"
     )
 
-    dskh5txt = f"{dataDir}/{sample}/dsk/{seg}.txt"
+    dskh5txt = f"{dataDir}/{sample}/dsk/{seg}.inter.txt"
     os.system(
         f"lib/dsk/build/bin/dsk2ascii \
         -nb-cores 0 \
         -file {dskH5} -out {dskh5txt}"
     )
 
-    dskh5txtSorted = f"{dataDir}/{sample}/dsk/{seg}.sorted"
+    dskh5txtSorted = f"{dataDir}/{sample}/dsk/{seg}.inter.sorted"
     os.system(
         f"sort -s -n -k 2 -nr {dskh5txt} > {dskh5txtSorted}"
     )
 
-    kmer1st = os.system(f"head -n 1 {dskh5txtSorted}")
-    return kmer1st
+    kmerDict = {}
+    with open(dskh5txtSorted, "r") as file:
+        for line in file.readlines()[0:3]:
+            kmer = line.split()[0]
+            kmerCount = line.split()[1]
+            kmerDict[kmer]=kmerCount
+    return kmerDict
 
-calculate_kmer(inputfile)
+n=0
+kmerCount = seqNum
+
+
+
+# for n in range(0,3):
+# # # while int(kmerCount) > int(seqNum*0.1):
+#     kmerList=[]
+#     kmerDict = calculate_kmer(inputfile)
+#     kmerList = [key for key in kmerDict]
+#     record_dict = SeqIO.to_dict(SeqIO.parse(inputfile, "fasta"))
+#
+#     boolean = [bool(re.search(kmerList[0], str(record_dict[record].seq), re.IGNORECASE)) for record in record_dict]
+#
+#     for kmer in kmerList:
+#
+#         if True in boolean:
+#             intermediate = f"{dataDir}/{sample}/dsk/{seg}.inter{n}.truncated"
+#             intermediate = open(intermediate, "w")
+#             for record in record_dict:
+#                 seq = str(record_dict[record].seq)
+#                 if re.search(kmer, seq, re.IGNORECASE) is None:
+#                     outputfile.write(f"{kmer}\t{kmerDict[kmer]}\n")
+#                 else:
+#                     intermediate.write(record_dict[record].format("fasta"))
+#                     outputfile.write(f"{kmer}\t{kmerDict[kmer]}\n")
+#             intermediate.close()
+#     inputfile = intermediate
+#     n += 1
+
+for n in range(0,3):
+    kmerList=[]
+    kmerDict = calculate_kmer(inputfile)
+    kmerList = [key for key in kmerDict]
+    record_dict = SeqIO.to_dict(SeqIO.parse(inputfile, "fasta"))
+    intermediate = open(f"{dataDir}/{sample}/dsk/{seg}.inter{n}.truncated", "w")
+
+    for kmer in kmerList[0:3]:
+        for record in record_dict:
+            seq = str(record_dict[record].seq)
+            boolean = [bool(re.search(kmer, seq, re.IGNORECASE)) for record in record_dict]
+            if True in boolean:
+                if re.search(kmer, seq, re.IGNORECASE) is None:
+                    outputfile.write(f"{kmer}\t{kmerDict[kmer]}\n")
+                    intermediate.write(record_dict[record].format("fasta"))
+    intermediate.close()
+    inputfile = f"{dataDir}/{sample}/dsk/{seg}.inter{n}.truncated"
+    n+=1
+
+    outputfile.close()
